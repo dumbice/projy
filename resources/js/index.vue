009ec0e09a708e6bed35ac8b8cafac56749bc03f
dumbice <gegle.bill@gmail.com>
2020-01-08 13:44:25 +0900

    # npm install vuex --save # npm install vue-router # npm install moment

---
+++ resources/js/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav class="navbar bg-white border-bottom navbar-light">
+
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
